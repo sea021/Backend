59f6419fd6336a7e091df6569cdd523035402b63
pee sukjai <peeile89@gmail.com>
2025-11-11 13:51:24 +0700

    add env

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+   port: process.env.DB_PORT, //เพิ่ม port
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
