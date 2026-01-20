33a57e92c6cafa165889f21bb45f3d58cc44b4f4
pee sukjai <peeile89@gmail.com>
2026-01-20 12:20:03 +0700

    changePort5000

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
   res.json({ status: 'Server is running', documentation: '/api-docs' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 module.exports = app;
 
 if (require.main === module) {
