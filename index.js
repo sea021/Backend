5fe98b0a0a1e006b0af575c8e44d895ecc028ad8
pee sukjai <peeile89@gmail.com>
2026-01-20 12:12:54 +0700

    fixx

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
 // --- Middleware CSP (แก้ปัญหาจอขาว) ---
 app.use((req, res, next) => {
   res.setHeader(
